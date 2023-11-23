f5ab683cb931455c6986cc110f9f8e2d8c34bcaa
Emil Olsen <emiols2003@skola.goteborg.se>
2023-11-23 18:48:23 +0100

    add all steps for assignment 4 (AOP-VG)

---
+++ src/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,23 @@
+package com.yrgo.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.framework.AopProxyUtils;
+
+
+public class PerformanceTimingAdvice {
+
+    public Object performTimingMeasurement(ProceedingJoinPoint method) throws Throwable {
+        long startTime = System.nanoTime();
+        try {
+            return method.proceed();
+        }
+        finally {
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            long timeTakenInMs = timeTaken / 1000000;
+            System.out.printf(
+                "Time taken for the method %s from the class %s took %dms%n",
+                method.getSignature().getName(), AopProxyUtils.ultimateTargetClass(method.getTarget()).getName(), timeTakenInMs);
+        }
+    }
+}
